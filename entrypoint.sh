e9ffd22217b5395fea330ef030cb6fae6dc7e798
Nicolas Bruegger <brueggern@gmail.com>
2020-02-13 10:25:34 +0100

    Create SSH key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -l
 
-composer install --prefer-dist
+eval $(ssh-agent -s)
 
-ssh-keyscan $1 >> ~/.ssh/known_hosts
+echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
+chmod 600 /tmp/id_rsa
+ssh-add /tmp/id_rsa
+
+composer install --prefer-dist
 
 composer run dep deploy production
