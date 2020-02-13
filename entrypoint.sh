82c32a8cbc8240b978cf556bb65b5d858819922e
Nicolas Bruegger <brueggern@gmail.com>
2020-02-13 10:32:08 +0100

    Yet another try

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 eval $(ssh-agent -s)
 
-echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
 ssh-add /tmp/id_rsa
+ssh-keyscan 212.147.34.198 >> ~/.ssh/known_hosts
 
 composer install --prefer-dist
 
