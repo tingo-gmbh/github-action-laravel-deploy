188ecd498a19f0558b2d1a36b17f84d0ed0ef11d
Nicolas Bruegger <brueggern@gmail.com>
2020-02-13 10:38:10 +0100

    Yet another try

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
 ssh-add /tmp/id_rsa
-ssh-keyscan 212.147.34.198 >> ~/.ssh/known_hosts
+ssh-keyscan $SSH_REMOTE_HOST >> ~/.ssh/known_hosts
 
 composer install --prefer-dist
+
+ls -la ~/.ssh
 
 composer run dep deploy production
